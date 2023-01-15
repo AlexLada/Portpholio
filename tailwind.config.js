cb9bd22900a4a3b75ceaf0ba452d6b805a98fc51
AlexLada <vizuvi2@mail.ru>
2023-01-15 23:28:08 +0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: ["./src/**/*.{html,js}","./public/index.html","./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}",],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
